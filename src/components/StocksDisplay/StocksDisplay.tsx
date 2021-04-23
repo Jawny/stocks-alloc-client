ece06d33fee8a8d6a9b9a2c8c68d0542c2bb137c
Johnny Liao <johnny@Johnnys-MacBook-Pro.local>
2021-04-22 18:31:47 -0700

    installed immer and lodash and added basic schema for sotcksdisplay component

--- src/components/StocksDisplay/StocksDisplay.tsx
+++ src/components/StocksDisplay/StocksDisplay.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { Table } from "antd";
+import produce from "immer";
+import StocksDisplaySchema from "./schema";
 import "./StocksDisplay.scss";
 
 const StocksDisplay = () => {
-  return <Table />;
+  const getData = () => {
+    return produce(fields);
+  };
+
+  const getFields = () => {
+    return produce(fields, (draft) => {
+      const firstField = draft[0];
+      _set(firstField, "label", title);
+      draft.shift();
+      draft.unshift(firstField);
+    });
+  };
+  return (
+    <Table
+      className="stock-display-table"
+      columns={StocksDisplaySchema.columns}
+      dataSource={}
+    />
+  );
 };
 
 export default StocksDisplay;
---
+++ src/components/StocksDisplay/schema.ts
@@ -0,0 +1,9 @@
+const StocksDisplaySchema = {
+  columns: [
+    { title: "Stock", dataIndex: "stock", key: "stock" },
+    { title: "Shares", dataIndex: "shares", key: "shares" },
+  ],
+  data: [],
+};
+
+export default StocksDisplaySchema;
